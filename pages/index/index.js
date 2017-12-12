55c46b24c482ecc7a4dad200c53848690526789d
张加涛 <xiaodi-zjt@mockuai.com>
2017-12-12 18:00:51 +0800

    change index test case

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,11 @@
 //index.js
 //获取应用实例
 const zToast = require('../../utils/zToast.js');
+const zPromisify = require('../../utils/zPromisify.js');
+const zx = zPromisify();
 
 Page({
   onLoad() {
-    zToast.info('成功', { type: 2 }).then(() => {
-      console.log('成功');
-    }).catch(() => {
-      console.log('失败');
-    });
+    console.log(zx);
   }
 });
